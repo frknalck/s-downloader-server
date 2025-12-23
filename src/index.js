d4cef6f6efc5946d8239eb490bdd5f4e48fe7053
Furkan Alçık <frknalck@gmail.com>
2025-12-23 20:08:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,6 +18,12 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   next();
 });
 
